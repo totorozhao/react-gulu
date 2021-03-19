6aede635a1faac93989319aed31c33e986c96cba
zhaojingjing <zhaojingjing@slwy.com>
2021-03-19 15:16:35 +0800

    添加css

--- src/icon/icon.tsx
+++ src/icon/icon.tsx
@@ -5,14 +5,15 @@ import React from "react"
 // import "../icons/orange.svg"
 import './importIcons'
 
+import './icon.scss'
 interface IconProps {
   name: string
 }
 
 const Icon: React.FunctionComponent<IconProps> = (props) => {
   return (
-    <span>
-      <svg>
+    <span >
+      <svg className='zui-icon'>
         <use xlinkHref={`#${props.name}`}></use>
       </svg>
     </span>
